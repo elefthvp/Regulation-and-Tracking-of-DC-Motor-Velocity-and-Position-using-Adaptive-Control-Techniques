2844d4d44b2ca20d3e976849a9ff76fd03ebbc84
elefthvp <bellapapaioannou@gmail.com>
2021-08-08 17:30:13 +0300

    readme addition, code beautification

--- Simulation/MRAC Controllers/Theta Control - Direct MRAC with Normalization/solvethetadot.m
+++ Simulation/MRAC Controllers/Theta Control - Direct MRAC with Normalization/solvethetadot.m
@@ -1,4 +1,5 @@
 function theta_new = solvethetadot(Gamma,epsilon,phi,theta_prev,sgn,t)
+%integration of the theta dot vector to decide its next value
     for j=1:length(Gamma)
     theta_dot=Gamma(j,j)*epsilon*phi(j)*sgn;
     temp = ode45(@(t,y)theta_dot,t,theta_prev(j));
@@ -6,5 +7,3 @@ function theta_new = solvethetadot(Gamma,epsilon,phi,theta_prev,sgn,t)
     end
 end
 
-%add sgn
-%ode45(@(t,y) -2*y+6,t,y0)
